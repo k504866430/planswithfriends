a4befc51077494a7161299976da21f13d83d8261
Kruttik Aggarwal <kruttikagarwal@gmail.com>
2011-12-03 17:44:14 -0500

    Final

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
   def trigger
     @socialize_plans = SocializePlan.all
     @socialize_plans.each{|s| SocializePlan.process_activity(s.identifier)}
-    render 'done'
   end
 
 end
